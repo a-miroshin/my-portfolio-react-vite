a650e9a55dca959447180a539d649f4844e2c34e
Alexander <alexmiroshinov@gmail.com>
2025-03-13 02:09:21 +0300

    Changed scrollbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+
 import Info from "@components/info/Info";
 import Gallery from "@components/gallery/Gallery";
 
@@ -7,8 +9,12 @@ const App = () => {
   const preferences = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const isDark = preferences;
 
+  useEffect(() => {
+    document.body.setAttribute("data-theme", isDark ? "dark" : "light");
+  }, [isDark]);
+
   return (
-    <div className="main__page" data-theme={isDark ? "dark" : "light"}>
+    <div className="main__page">
       <Info />
       <Gallery />
     </div>
